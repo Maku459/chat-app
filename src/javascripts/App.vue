dfcbe9f98a9a76277686b88c68fe8c1d805947c6
Kyoko Sakuma <>
2019-03-17 20:25:18 +0900

    クライアント側実装

--- src/javascripts/App.vue
+++ src/javascripts/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <p>
-      <img class="logo" src="../images/logo.jpg" alt="ロゴ">
-      <span class="sample">テスト</span>
+      <span class="sample">チャット</span>
     </p>
-    <MyComponent :messageList="$data.messageList" />
+    <ul>
+        <li
+          v-for="item in $data.messageList"
+          :key="item.id"
+        >
+          <span class="list__name">{{ item.name }}</span>
+          <span class="list__item">{{ item.text }}</span>
+        </li>
+      </ul>
     <form @submit="onSubmit">
+      名前：
+      <input v-model="$data.name" type="text">
+      テキスト：
       <input v-model="$data.text" type="text">
       <button type="submit">送信</button>
     </form>
@@ -23,39 +33,41 @@ export default {
   data() {
     const messageList = [
       {
+        name: 'a',
         text: 'アイウエオ'
       }
     ];
     return {
+      // map: 配列内のすべての要素に処理を行い、その戻り値から新しい配列を作成する
       messageList: messageList.map((item, index) => ({ ...item, id: index })),
       nextMessageId: messageList.length,
-      message: '',
+      name: '',
       text: ''
     };
   },
   created() {
+    // 受け取る
     socket.on('connect', () => {
       console.log('connected!');
+      // this.$data.text = 'connected';
+      // socket.emit('connect', this.onSubmit());
     });
-    socket.on('send', (message) => {
-      console.log(message);
-      this.$data.message = message;
-    });
+    // socket.on('send', (message) => {
+    //   console.log(message);
+    //   this.$data.message = message;
+    // });
   },
   methods: {
     /**
      * Enterボタンを押したとき
      */
-    // onSubmit(e) {
-    //   e.preventDefault();
-    //   socket.emit('send', this.$data.text);
-    // },
-
     onSubmit(e) {
+      // フォーム遷移を防ぐ
       e.preventDefault();
-      socket.emit('send', this.$data.text);
-      this.$data.messageList.unshift({
+      socket.emit('send', { name: this.$data.name, text: this.$data.text });
+      this.$data.messageList.push({
         id: this.$data.nextMessageId,
+        name: this.$data.name,
         text: this.$data.text
       });
       this.$data.nextMessageId += 1;
@@ -65,11 +77,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.logo {
-  width: 40px;
-}
-
 .sample {
   color: $red;
 }
+
+li {
+  position: relative;
+  list-style: none;
+  padding-top: 15px;
+  padding-bottom: 15px;
+
+  .list__name {
+    position: relative;
+    padding-right: 10px;
+  }
+
+  .list__item {
+    position: relative;
+    box-sizing: border-box;
+    padding: 10px;
+    border-radius: 5px;
+    background-color: yellowgreen;
+
+    &::before {
+      position: absolute;
+      content: '';
+      top: 50%;
+      left: -14px;
+      margin-top: -7px;
+      border: 7px solid transparent;
+      border-right: 7px solid yellowgreen;
+    }
+  }
+}
+
+form {
+  position: fixed;
+  bottom: 0;
+  padding: 10px;
+}
+
 </style>

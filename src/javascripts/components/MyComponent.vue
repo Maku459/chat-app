1d55b3287b3e375c99f6d6538eac2da72dbb55e9
Kyoko Sakuma <>
2019-03-25 14:55:36 +0900

    不要部分削除

--- src/javascripts/components/MyComponent.vue
+++ src/javascripts/components/MyComponent.vue
@@ -1,16 +1,5 @@
-<template v-for="item in $props.messageList">
-        <li
-          :key="item.id"
-          class="item"
-          @click="$emit('check', item.id)"
-        >
-          <span class="item__text">{{ item.text }}</span>
-        </li>
-      </template>
-
 <script>
 import VueTypes from 'vue-types';
-
 export default {
   props: {
     messageList: VueTypes.arrayOf(VueTypes.shape({
